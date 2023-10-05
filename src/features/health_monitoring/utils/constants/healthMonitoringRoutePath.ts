519a294bfc58ce0df43a2f145ec0120fd8088be6
Sinut Wattanarporn <s_w.piepie@outlook.com.au>
2023-10-05 21:32:00 +0700

    feat: CACE-0 server sent event api response

---
+++ src/features/health_monitoring/utils/constants/healthMonitoringRoutePath.ts
@@ -0,0 +1,3 @@
+export default class HealthMonitoringRoutePath {
+  public static watchHealth = "/api/v1/health/:id";
+}
--- src/utils/functions/apiResponse.ts
+++ src/utils/functions/apiResponse.ts
@@ -1,7 +1,10 @@
-import { Response } from "express";
+import { Request, Response } from "express";
 
 export default class APIResponse {
   private constructor() {}
+
+  /// RESTFul
+  /// __________________
 
   /// 200-299
 
@@ -16,5 +19,29 @@ export default class APIResponse {
 
   public static unAuthorized<T>(res: Response, body: T | null) {
     res.status(401).send(body);
+  }
+
+  /// Server-Sent Event
+  /// __________________
+
+  public static serverSentEvent(
+    req: Request,
+    res: Response,
+    writtingCallback: () => void,
+    onCloseCallback?: () => void
+  ) {
+    res.set({
+      "Cache-Control": "no-cache",
+      "Content-Type": "text/event-stream",
+      Connection: "keep-alive",
+    });
+    res.flushHeaders();
+
+    writtingCallback();
+
+    req.on("close", () => {
+      if (onCloseCallback !== undefined) onCloseCallback();
+      res.end();
+    });
   }
 }
